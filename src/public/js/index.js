f7a1023659553e7d0d5860017e45f67932dfed70
MercadoEduardo <eduardo@fichap.com>
2023-09-05 23:19:25 -0300

    webSockets

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,18 @@
-console.log('hola');
+const socket = io();
+
+socket.emit("message", "hola desde websocket");
+const productContainer = document.getElementById("products");
+
+socket.on("products", (products) => {
+  const title = products.map((product) => product.title);
+
+  productContainer.innerHTML = title.join("<br></br>");
+});
+
+socket.on("Put_products", (products) => {
+  const title = products.map((product) => product.title);
+
+  productContainer.innerHTML = title.join("<br></br>");
+});
+
+console.log("hola dev");
--- src/routes/View.Routes.js
+++ src/routes/View.Routes.js
@@ -1,9 +1,25 @@
-import express, { Router } from "express";
+import express from "express";
+import { Router } from "express";
+import ProductManager from "../ProductManager-2.js";
 
-const router = express.Router();
+const router = Router();
+const productManager = new ProductManager();
 
-router.get("/", (req, res) => {
-  res.render("index", {});
+router.get("/", async (req, res) => {
+  req.context.socketServer.emit();
+  const products = await productManager.getProducts();
+  res.render("home", { products });
+});
+
+router.get("/realtimeproducts", async (req, res) => {
+  try {
+    req.context.socketServer.emit(); 
+    const products = await productManager.getProducts();
+    res.render("realtimeproducts", { products });
+  } catch (error) {
+    console.error("Error en la ruta '/realtimeproducts':", error);
+    res.status(500).send("Ocurrió un error en la solicitud.");
+  }
 });
 
 export default router;
